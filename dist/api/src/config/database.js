591611db2caaf3f3cb1ba7b33c8c0a6a24d113a0
unknown <eduardo@codantdigital.com>
2024-05-06 14:04:11 -0600

    algo

--- dist/api/src/config/database.js
+++ dist/api/src/config/database.js
@@ -15,7 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
-const uri = `mongodb+srv://eduardobalinaslira:5N9qjUf1CPNYxnDH@cluster0.c8plphw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb+srv://eduardobalinaslira:5N9qjUf1CPNYxnDH@cluster0.c8plphw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 //const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CONNECTION}/?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`;
 const client = new MongoClient(uri, {
     serverApi: {
@@ -24,9 +24,11 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+console.log(client);
 const getCollections = (dbName, collectionName) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         yield client.connect();
+        console.log("Llego a la conexion");
         yield client.db("admin").command({ ping: 1 });
         const db = yield client.db(dbName);
         const collection = yield db.collection(collectionName);
--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 import dotenv from "dotenv";
 
 dotenv.config();
-const uri = `mongodb+srv://eduardobalinaslira:5N9qjUf1CPNYxnDH@cluster0.c8plphw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = "mongodb+srv://eduardobalinaslira:5N9qjUf1CPNYxnDH@cluster0.c8plphw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 //const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CONNECTION}/?retryWrites=true&w=majority&appName=${process.env.DB_APP_NAME}`;
 
 const client = new MongoClient(uri, {
@@ -14,9 +14,14 @@ const client = new MongoClient(uri, {
   }
 });
 
+console.log(client);
+
+
 const getCollections = async ( dbName: string, collectionName: string ) => {
     try {
         await client.connect();
+        console.log("Llego a la conexion");
+        
         await client.db("admin").command({ ping: 1 });
         
         const db = await client.db(dbName);
